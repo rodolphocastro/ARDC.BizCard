e0a5365c0feca1d5383cad1f32554a160eebf489
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-05-26 18:15:16 -0300

    Adicionar model de Cartão de Negócios

---
+++ ARDC.BizCard.Core/Models/BizCard.cs
@@ -0,0 +1,19 @@
+﻿namespace ARDC.BizCard.Core.Models
+{
+    public class BizCardContent
+    {
+        public string NomeCompleto { get; set; }
+
+        public string Email { get; set; }
+
+        public string TelefonePrincipal { get; set; }
+
+        public string TelefoneSecundario { get; set; }
+
+        public string Whatsapp { get; set; }
+
+        public string LinkedIn { get; set; }
+
+        public string Website { get; set; }
+    }
+}
