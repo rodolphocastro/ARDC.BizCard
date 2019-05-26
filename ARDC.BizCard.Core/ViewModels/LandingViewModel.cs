27e7ccd3b526df9e62265bcd012788cc4bbcaa21
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-05-26 18:40:34 -0300

    Adicionar ViewModel de Landing

---
+++ ARDC.BizCard.Core/ViewModels/LandingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Logging;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+
+namespace ARDC.BizCard.Core.ViewModels
+{
+    public class LandingViewModel : MvxNavigationViewModel
+    {
+        public LandingViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
+        {
+        }
+
+        public IMvxAsyncCommand NavigateToCardCommand { get; private set; }
+
+        public IMvxAsyncCommand NavigateToEditCommand { get; private set; }
+    }
+}
