a029cf36c8a5eb6db169f89b9fb39220b5cac48b
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-05-26 19:40:55 -0300

    Adicionar implementação do serviço de BizCards

---
+++ ARDC.BizCard.Core/Services/BizCardService.cs
@@ -0,0 +1,29 @@
+﻿using ARDC.BizCard.Core.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ARDC.BizCard.Core.Services
+{
+    public class BizCardService : IBizCardService
+    {
+        private BizCardContent BizCard { get; set; }
+
+        public Task CreateOrEditCardAsync(BizCardContent bizCard, CancellationToken ct)
+        {
+            BizCard = bizCard;
+
+            return Task.CompletedTask;
+        }
+
+        public Task<BizCardContent> GetCardAsync(CancellationToken ct)
+        {
+            return Task.FromResult(BizCard);
+        }
+
+        public Task GetQRCodeAsync(BizCardContent bizCard, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
