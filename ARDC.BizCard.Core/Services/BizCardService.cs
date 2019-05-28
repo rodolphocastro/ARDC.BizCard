f115a79d6b61cacad195ca389809fa1bd3f2af97
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-28 16:53:42 -0300

    Refatorar BizCardService para utilizar o Serviço de Cache
    Fixes #5

--- ARDC.BizCard.Core/Services/BizCardService.cs
+++ ARDC.BizCard.Core/Services/BizCardService.cs
@@ -10,27 +10,30 @@ namespace ARDC.BizCard.Core.Services
 {
     public class BizCardService : IBizCardService
     {
+        private const string MyBizCardCacheKey = "my_bizcard";
+
         private BizCardContent BizCard { get; set; }
+
+        private ICacheService CacheService { get; }
+
+        public BizCardService(ICacheService cacheService)
+        {
+            CacheService = cacheService ?? throw new ArgumentNullException(nameof(cacheService));
+        }
 
         public async Task CreateOrEditCardAsync(BizCardContent bizCard, CancellationToken ct)
         {
             BizCard = bizCard;
 
-            await BlobCache.UserAccount.InsertObject("my_biz_card", BizCard);
+            await CacheService.StoreObjectAsync(MyBizCardCacheKey, BizCard, CacheType.Local);
         }
 
         public async Task<BizCardContent> GetCardAsync(CancellationToken ct)
         {
-            try
-            {
-                BizCard = await BlobCache.UserAccount.GetObject<BizCardContent>("my_biz_card");
-            }
-            catch (KeyNotFoundException)
-            {
-                BizCard = new BizCardContent();
-            }
+            if (BizCard == null)
+                BizCard = await CacheService.RecoverObjectAsync<BizCardContent>(MyBizCardCacheKey, CacheType.Local);
 
-            return BizCard;
+            return BizCard ?? new BizCardContent();
         }
 
         public Task GetQRCodeAsync(BizCardContent bizCard, CancellationToken ct)
