feab98bb02a4ba394cc1a0aae3c5f7aa821cd522
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-05-29 22:47:02 -0300

    Adicionar ViewModel e Fragment para o conteúdo do Landing

---
+++ ARDC.BizCard.Droid/Fragments/HomeContent.cs
@@ -0,0 +1,22 @@
+﻿using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using ARDC.BizCard.Core.ViewModels;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+
+namespace ARDC.BizCard.Droid.Fragments
+{
+    [MvxFragmentPresentation(ActivityHostViewModelType = typeof(LandingViewModel), AddToBackStack = true, FragmentContentId = Resource.Id.content_frame)]
+    [Register(nameof(HomeContent))]
+    public class HomeContent : MvxFragment<HomeContentViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var _ = base.OnCreateView(inflater, container, savedInstanceState);
+
+            return this.BindingInflate(Resource.Layout.content_main, null);
+        }
+    }
+}
