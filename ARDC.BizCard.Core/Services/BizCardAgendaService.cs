e0cf78f6d50a8b0fdfa7023de9020a427d205f17
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-06-02 20:05:14 -0300

    Adicionar Service para acesso á agenda de Cartões
    Related #20

---
+++ ARDC.BizCard.Core/Services/BizCardAgendaService.cs
@@ -0,0 +1,53 @@
+﻿using ARDC.BizCard.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ARDC.BizCard.Core.Services
+{
+    public class BizCardAgendaService : IBizCardAgendaService
+    {
+        private const string MyAgendaCacheKey = "my_agenda";
+
+        private List<BizCardContent> BizCards { get; set; }
+
+        private ICacheService CacheService { get; }
+
+        public BizCardAgendaService(ICacheService cacheService)
+        {
+            CacheService = cacheService ?? throw new ArgumentNullException(nameof(cacheService));
+        }
+
+        public async Task AddCardAsync(BizCardContent newCard, CancellationToken ct)
+        {
+            if (BizCards == null)
+                await InitializeCollection();
+
+            BizCards.Add(newCard);  // TODO: Validar duplicatas
+        }
+
+        public async Task<IList<BizCardContent>> GetCardsAsync(CancellationToken ct)
+        {
+            if (BizCards == null)
+                await InitializeCollection();
+
+            return BizCards;
+        }
+
+        public async Task RemoveCardAsync(BizCardContent card, CancellationToken ct)
+        {
+            if (BizCards == null)
+                await InitializeCollection();
+
+            BizCards.Remove(card); // TODO: Validar casos inexistentes
+        }
+
+        private async Task InitializeCollection()
+        {
+            BizCards = new List<BizCardContent>();
+            var cacheCards = await CacheService.RecoverObjectAsync<List<BizCardContent>>(MyAgendaCacheKey, CacheType.Local);
+            BizCards.AddRange(cacheCards);
+        }
+    }
+}
