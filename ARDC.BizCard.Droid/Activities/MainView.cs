3304ffe8c4d83460567438949c4fe3eb53cbaa39
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-06-02 00:40:30 -0300

    Refatorar nome do layout da Main Activity
    Fixes #22

--- ARDC.BizCard.Droid/Activities/MainView.cs
+++ ARDC.BizCard.Droid/Activities/MainView.cs
@@ -16,7 +16,7 @@ namespace ARDC.BizCard.Droid.Activities
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.main);
 
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
