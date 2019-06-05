597ee6041a848a99d85134357f8b0ed384bea449
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-06-05 13:54:53 -0300

    Adicionar dependência à Acr.UserDialogs
    Related #8

--- ARDC.BizCard.Droid/Setup.cs
+++ ARDC.BizCard.Droid/Setup.cs
@@ -1,10 +1,13 @@
-﻿using Android.Support.Design.Button;
+﻿using Acr.UserDialogs;
+using Android.Support.Design.Button;
 using Android.Support.Design.Widget;
 using Android.Support.V4.Widget;
 using Android.Support.V7.Widget;
 using ARDC.BizCard.Core;
+using MvvmCross;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.Platforms.Android;
 using MvvmCross.Plugin;
 using MvvmCross.ViewModels;
 using System.Collections.Generic;
@@ -17,6 +20,7 @@ namespace ARDC.BizCard.Droid
         protected override void InitializeApp(IMvxPluginManager pluginManager, IMvxApplication app)
         {
             base.InitializeApp(pluginManager, app);
+            UserDialogs.Init(() => Mvx.IoCProvider.Resolve<IMvxAndroidCurrentTopActivity>().Activity);
         }
         protected override IEnumerable<Assembly> AndroidViewAssemblies => new List<Assembly>(base.AndroidViewAssemblies)
         {
