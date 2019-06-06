9b1431f21fc286c6f24a3aad0e136c5a2c2dae3d
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-06-05 21:47:35 -0300

    Remover chamada ao UserDialogs, que não estava respondendo ao contexto Async

--- ARDC.BizCard.Core/ViewModels/QR/QrCodeScannerViewModel.cs
+++ ARDC.BizCard.Core/ViewModels/QR/QrCodeScannerViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Acr.UserDialogs;
-using ARDC.BizCard.Core.Models;
+﻿using ARDC.BizCard.Core.Models;
 using ARDC.BizCard.Core.Services;
 using ARDC.BizCard.Core.ViewModels.Card;
 using MvvmCross.Commands;
@@ -14,16 +13,14 @@ namespace ARDC.BizCard.Core.ViewModels.QR
 {
     public class QrCodeScannerViewModel : MvxNavigationViewModel
     {
-        public QrCodeScannerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IBizCardService bizCardService, IUserDialogs userDialogsService) : base(logProvider, navigationService)
+        public QrCodeScannerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IBizCardService bizCardService) : base(logProvider, navigationService)
         {
             BizCardService = bizCardService ?? throw new ArgumentNullException(nameof(bizCardService));
-            UserDialogsService = userDialogsService ?? throw new ArgumentNullException(nameof(userDialogsService));
 
             ReadCardCommand = new MvxCommand<string>((p) => SaveCardTask = MvxNotifyTask.Create(() => LoadCardFromJsonAsync(p)));
         }
 
         private IBizCardService BizCardService { get; }
-        private IUserDialogs UserDialogsService { get; }
 
         private MvxNotifyTask _saveCardTask;
 
@@ -40,15 +37,7 @@ namespace ARDC.BizCard.Core.ViewModels.QR
             var bizCard = await BizCardService.GetCardFromJSONAsync(payload, ct);
 
             if (bizCard.HasData())
-            {
-                UserDialogsService.Toast("Cartão lido", TimeSpan.FromSeconds(2));
-
                 await NavigationService.Navigate<ViewCardViewModel, BizCardContent>(bizCard);
-            }
-            else
-            {
-                UserDialogsService.Toast("Não foi processar o QR Code");
-            }            
         }
     }
 }
