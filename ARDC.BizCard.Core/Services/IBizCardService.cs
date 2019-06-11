5f5f76eb5d2018e088c0e81b70df41316d837f46
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-06-11 17:33:15 -0300

    Adicionar documentação do serviço de BizCard
    Related #16

--- ARDC.BizCard.Core/Services/IBizCardService.cs
+++ ARDC.BizCard.Core/Services/IBizCardService.cs
@@ -4,16 +4,46 @@ using System.Threading.Tasks;
 
 namespace ARDC.BizCard.Core.Services
 {
+    /// <summary>
+    /// Define os métodos para acesso ao cartão do usuário.
+    /// </summary>
     public interface IBizCardService
     {
+        /// <summary>
+        /// Criar ou Editar o cartão.
+        /// </summary>
+        /// <param name="bizCard">Os dados a serem criados/atualizados</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
         Task CreateOrEditMyCardAsync(BizCardContent bizCard, CancellationToken ct = default);
 
+        /// <summary>
+        /// Recuperar o cartão.
+        /// </summary>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>O Cartão do usuário</returns>
+        /// <remarks>Caso o cartão ainda nào exista um cartão vazio deve ser retornado</remarks>
         Task<BizCardContent> GetMyCardAsync(CancellationToken ct = default);
 
+        /// <summary>
+        /// Recupera uma representação JSON do cartão.
+        /// </summary>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>Uma string contendo o cartão serializado em padrão JSON</returns>
         Task<string> GetMyCardAsJSONAsync(CancellationToken ct = default);
 
-        Task<BizCardContent> GetCardFromJSONAsync(string jsonCard, CancellationToken ct = default);
+        /// <summary>
+        /// Recupera um cartão a partir de um JSON.
+        /// </summary>
+        /// <param name="jsonCard">JSON contendo o cartão a ser recuperado</param>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>O Cartão desserializado</returns>
+        Task<BizCardContent> GetCardFromJSONAsync(string jsonCard, CancellationToken ct = default);     // TODO: Refatorar para que este método pertença ao IBizCardAgendaService
 
+        /// <summary>
+        /// Recupera o Gravatar (Imagem) do usuário.
+        /// </summary>
+        /// <param name="ct">Token para controle de cancelamento</param>
+        /// <returns>Array de bytes contendo a imagem do Gravatar</returns>
         Task<byte[]> GetGravatarAsync(CancellationToken ct = default);
     }
 }
