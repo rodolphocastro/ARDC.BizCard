51df53ec08d19f613b33438b4b2a1b76405b7c02
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-06-11 22:39:50 -0300

    Adicionar documentação a ViewModels Raiz
    Related #16

--- ARDC.BizCard.Core/ViewModels/LandingViewModel.cs
+++ ARDC.BizCard.Core/ViewModels/LandingViewModel.cs
@@ -6,13 +6,24 @@ using MvvmCross.ViewModels;
 
 namespace ARDC.BizCard.Core.ViewModels
 {
+    /// <summary>
+    /// ViewModel para Landing do App, primeira tela de conteúdo do App.
+    /// </summary>
     public class LandingViewModel : MvxNavigationViewModel
     {
+        /// <summary>
+        /// Cria uma nova instância da LandingViewModel.
+        /// </summary>
+        /// <param name="logProvider">Provedor de logs a ser utilizado</param>
+        /// <param name="navigationService">Provedor de navegação a ser utilizado</param>
         public LandingViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService) : base(logProvider, navigationService)
         {
             NavigateToCardCommand = new MvxAsyncCommand(async () => await NavigationService.Navigate<ViewMyCardViewModel>());
         }
 
+        /// <summary>
+        /// Command para navegar á ViewModel de "Meu Cartão".
+        /// </summary>
         public IMvxAsyncCommand NavigateToCardCommand { get; private set; }
     }
 }
