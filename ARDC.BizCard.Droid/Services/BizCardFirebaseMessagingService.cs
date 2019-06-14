cfb356ae3c80a0a8642ba51cc8db7f0ac0e888f1
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-06-14 16:34:42 -0300

    Adicionar serviço para mensagens em Foreground

---
+++ ARDC.BizCard.Droid/Services/BizCardFirebaseMessagingService.cs
@@ -0,0 +1,22 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace ARDC.BizCard.Droid.Services
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class BizCardFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "BizCardFirebaseMessaging";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+        }
+
+    }
+}
