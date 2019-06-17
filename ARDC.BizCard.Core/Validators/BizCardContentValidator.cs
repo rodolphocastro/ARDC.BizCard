9eb5b926241d15f3dbd42eb4bd8c8dcdbbdaaaf8
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-06-16 21:05:15 -0300

    Adicionar validação ao NomeCompleto
    Related #38

---
+++ ARDC.BizCard.Core/Validators/BizCardContentValidator.cs
@@ -0,0 +1,14 @@
+﻿using ARDC.BizCard.Core.Models;
+using FluentValidation;
+
+namespace ARDC.BizCard.Core.Validators
+{
+    public class BizCardContentValidator : AbstractValidator<BizCardContent>
+    {
+        public BizCardContentValidator()
+        {
+            // Nome Completo: Deve estar preenchido, entre 5 e 255 caracteres
+            RuleFor(c => c.NomeCompleto).NotEmpty().MinimumLength(5).MaximumLength(255);
+        }
+    }
+}
