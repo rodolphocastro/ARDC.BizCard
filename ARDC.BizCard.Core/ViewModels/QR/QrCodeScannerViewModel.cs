afd310d1cc4c3d49d7d4f3504693f7e48fe8e437
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-06-18 22:41:29 -0300

    Exibir Alert ao ler um QR Code de BizCard com Mensagem
    Related #51

--- ARDC.BizCard.Core/ViewModels/QR/QrCodeScannerViewModel.cs
+++ ARDC.BizCard.Core/ViewModels/QR/QrCodeScannerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ARDC.BizCard.Core.Models;
+﻿using Acr.UserDialogs;
+using ARDC.BizCard.Core.Models;
 using ARDC.BizCard.Core.Services;
 using ARDC.BizCard.Core.ViewModels.Card;
 using MvvmCross.Commands;
@@ -22,10 +23,12 @@ namespace ARDC.BizCard.Core.ViewModels.QR
         /// <param name="logProvider">Provedor de logs a ser utilizado</param>
         /// <param name="navigationService">Provedor de navegação a ser utilizado</param>
         /// <param name="bizCardService">Provedor de acesso a card a ser utilizado</param>
-        public QrCodeScannerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IBizCardAgendaService bizCardAgendaService) : base(logProvider, navigationService)
+        public QrCodeScannerViewModel(IMvxLogProvider logProvider, IMvxNavigationService navigationService, IBizCardAgendaService bizCardAgendaService, IUserDialogs userDialogsService) : base(logProvider, navigationService)
         {
             BizCardAgendaService = bizCardAgendaService ?? throw new ArgumentNullException(nameof(bizCardAgendaService));
+            UserDialogsService = userDialogsService ?? throw new ArgumentNullException(nameof(userDialogsService));
 
+            ShowCardMessageCommand = new MvxCommand<BizCardContent>((c) => MessageTask = MvxNotifyTask.Create(() => ShowMessageAsync(c)), (c) => CanShowMessage(c));
             ReadCardCommand = new MvxCommand<string>((p) => SaveCardTask = MvxNotifyTask.Create(() => LoadCardFromJsonAsync(p)));
         }
 
@@ -33,6 +36,11 @@ namespace ARDC.BizCard.Core.ViewModels.QR
         /// Provedor de Agenda.
         /// </summary>
         private IBizCardAgendaService BizCardAgendaService { get; }
+
+        /// <summary>
+        /// Provedor de Dialogs com o usuário.
+        /// </summary>
+        private IUserDialogs UserDialogsService { get; }
 
         private MvxNotifyTask _saveCardTask;
 
@@ -45,10 +53,26 @@ namespace ARDC.BizCard.Core.ViewModels.QR
             set { SetProperty(ref _saveCardTask, value); }
         }
 
+        private MvxNotifyTask _messageTask;
+
+        /// <summary>
+        /// Task para acompanhamento da exibição da mensagem de compartilhamento.
+        /// </summary>
+        public MvxNotifyTask MessageTask
+        {
+            get { return _messageTask; }
+            set { SetProperty(ref _messageTask, value); }
+        }
+
         /// <summary>
         /// Command para realizar a leitura do Card.
         /// </summary>
         public IMvxCommand<string> ReadCardCommand { get; private set; }
+
+        /// <summary>
+        /// Command para exibir a mensagem compartilhada pelo Cartão.
+        /// </summary>
+        public IMvxCommand<BizCardContent> ShowCardMessageCommand { get; private set; }
 
         /// <summary>
         /// Recupera um BizCardContent a partir de um JSON.
@@ -60,7 +84,21 @@ namespace ARDC.BizCard.Core.ViewModels.QR
             var bizCard = await BizCardAgendaService.GetCardFromJSONAsync(payload, ct);
 
             if (bizCard.HasData())
+            {
+                ShowCardMessageCommand.Execute(bizCard);
                 await NavigationService.Navigate<ViewCardViewModel, BizCardContent>(bizCard);
+            }
         }
+
+        /// <summary>
+        /// Verifica se a Mensagem pode ser exibida ou não.
+        /// </summary>
+        /// <returns>TRUE caso exista uma mensagem a ser exibida</returns>
+        private bool CanShowMessage(BizCardContent card) => !string.IsNullOrWhiteSpace(card.Mensagem);
+
+        /// <summary>
+        /// Exibe a mensagem do Cartão.
+        /// </summary>
+        private async Task ShowMessageAsync(BizCardContent card) => await UserDialogsService.AlertAsync(card.Mensagem, card.NomeCompleto);
     }
 }
