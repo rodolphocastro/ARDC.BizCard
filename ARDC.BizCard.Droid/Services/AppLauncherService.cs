e016960a3823d562b83f8ea4af36589974602d84
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-06-19 16:15:03 -0300

    Adicionar método para enviar email
    Related #45

--- ARDC.BizCard.Droid/Services/AppLauncherService.cs
+++ ARDC.BizCard.Droid/Services/AppLauncherService.cs
@@ -3,7 +3,6 @@ using Android.Content;
 using ARDC.BizCard.Core.Services;
 using MvvmCross;
 using MvvmCross.Platforms.Android;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,13 +19,20 @@ namespace ARDC.BizCard.Droid.Services
 
         public Task LaunchEMailAsync(string mailAddress, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            var intent = new Intent(Intent.ActionSend);
+            intent.SetType("*/*");
+            intent.PutExtra(Intent.ExtraEmail, mailAddress);
+
+            _appActivity.StartActivity(intent);
+
+            return Task.CompletedTask;
         }
 
         public Task LaunchLinkedInAsync(string profile, CancellationToken ct)
         {
             var uri = Android.Net.Uri.Parse($"https://linkedin.com/in/{profile}");
             var intent = new Intent(Intent.ActionView, uri);
+
             _appActivity.StartActivity(intent);
 
             return Task.CompletedTask;
