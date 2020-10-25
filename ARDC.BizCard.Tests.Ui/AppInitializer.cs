b57c50e765115cde4829b83b9aab51e23875934f
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-10-25 17:16:17 -0300

    chore: Scaffold UITest project

---
+++ ARDC.BizCard.Tests.Ui/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.UITest;
+
+namespace ARDC.BizCard.Tests.Ui
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    .InstalledApp("com.ARDC.BizCard.Droid")
+                    .StartApp();
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ ARDC.BizCard.Tests.Ui/Tests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+using System.Linq;
+
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace ARDC.BizCard.Tests.Ui
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            app.Screenshot("Welcome screen.");
+
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
